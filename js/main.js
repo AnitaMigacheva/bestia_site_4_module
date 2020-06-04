a6e8f2c50f0b7161a5a4daa951750ea88df34f88
anitamigacheva <anitamigacheva@gmail.com>
2020-06-04 20:35:44 +0300

    k

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 
-let burger = document.querySelector('.burger');
-let menu = document.querySelector('.menu');
+let burger = document.querySelector('.burger')
+let menu = document.querySelector('.menu')
+let page =document.getElementsByTagName('body')[0];
 
 burger.addEventListener('click', function() {
-burger.classList.toggle('burger-open');
+burger.classList.toggle('burger-open')
 if (burger.className == 'burger burger-open') {
-  menu.classList.toggle('menu-open');
-} else {
-  menu.classList.remove('menu-open');
-}
-});
+    menu.classList.toggle('menu-open')
+    page.classList.add('noscroll');
+  } else {
+  menu.classList.remove('menu-open')
+  }
+})
