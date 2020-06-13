1f81ccdb4703e226bed4f3ebcde8ab9a2e7d54ad
anitamigacheva <anitamigacheva@gmail.com>
2020-06-13 19:46:37 +0300

    -

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 
 let burger = document.querySelector('.burger')
 let menu = document.querySelector('.menu')
-let page =document.getElementsByTagName('body')[0]
+let page = document.getElementsByTagName('body')[0]
+let icons = document.querySelector('.header_top_icons')
 
 burger.addEventListener('click', function() {
 burger.classList.toggle('burger-open')
 if (burger.className == 'burger burger-open') {
     menu.classList.toggle('menu-open')
     page.classList.add('noscroll')
+    icons.style.background = 'none'
   } else {
     menu.classList.remove('menu-open')
     page.classList.remove('noscroll')
